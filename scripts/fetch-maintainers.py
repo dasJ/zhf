66d04c72648dd62bc47980dd6abca1922f82e448
Janne Heß <janne@hess.ooo>
2025-12-28 13:16:07 +0100

    Fix gitpython

--- scripts/fetch-maintainers.py
+++ scripts/fetch-maintainers.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i python3 -p python3 python3.pkgs.multiprocess python3.pkgs.GitPython
+#!nix-shell -i python3 -p python3 python3.pkgs.multiprocess python3.pkgs.gitpython
 
 
 
